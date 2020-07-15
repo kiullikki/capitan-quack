d0402c502687aeda3aaf24649de94734dc047e3b
kiullikki <kiullikki@gmail.com>
2020-07-15 03:03:30 +0300

    added adaptive

--- src/components/AddCarForm/index.tsx
+++ src/components/AddCarForm/index.tsx
@@ -55,43 +55,40 @@ export const AddCarForm = (props: IProps) => {
         isValid,
       }) => (
         <form onSubmit={handleSubmit} className={`${className} form`}>
-          <div className="form__row">
+          <CustomInput
+            className="form__name"
+            onChange={handleChange}
+            name={FIELD_TYPE.TITLE}
+            label={DICTIONARY[FIELD_TYPE.TITLE]}
+            value={values[FIELD_TYPE.TITLE]}
+            isTouched={touched[FIELD_TYPE.TITLE]}
+            error={errors[FIELD_TYPE.TITLE]}
+            isValid={!errors[FIELD_TYPE.TITLE]}
+          />
+          <CustomSelect
+            className="form__year"
+            options={createYearOptions(MIN_YEAR_CAR)}
+            name={FIELD_TYPE.YEAR}
+            label={DICTIONARY[FIELD_TYPE.YEAR]}
+            value={values[FIELD_TYPE.YEAR]}
+            setFieldValue={setFieldValue}
+            isTouched={touched[FIELD_TYPE.YEAR]}
+            error={errors[FIELD_TYPE.YEAR]}
+            isValid={!errors[FIELD_TYPE.YEAR]}
+          />
+          <CustomInput
+            className="form__price"
+            onChange={handleChange}
+            name={FIELD_TYPE.PRICE}
+            label={DICTIONARY[FIELD_TYPE.PRICE]}
+            value={values[FIELD_TYPE.PRICE]}
+            isTouched={touched[FIELD_TYPE.PRICE]}
+            error={errors[FIELD_TYPE.PRICE]}
+            isValid={!errors[FIELD_TYPE.PRICE]}
+            type="number"
+          />
+          <div className="form__description">
             <CustomInput
-              className="form__input"
-              onChange={handleChange}
-              name={FIELD_TYPE.TITLE}
-              label={DICTIONARY[FIELD_TYPE.TITLE]}
-              value={values[FIELD_TYPE.TITLE]}
-              isTouched={touched[FIELD_TYPE.TITLE]}
-              error={errors[FIELD_TYPE.TITLE]}
-              isValid={!errors[FIELD_TYPE.TITLE]}
-            />
-            <CustomSelect
-              className="form__select"
-              options={createYearOptions(MIN_YEAR_CAR)}
-              name={FIELD_TYPE.YEAR}
-              label={DICTIONARY[FIELD_TYPE.YEAR]}
-              value={values[FIELD_TYPE.YEAR]}
-              setFieldValue={setFieldValue}
-              isTouched={touched[FIELD_TYPE.YEAR]}
-              error={errors[FIELD_TYPE.YEAR]}
-              isValid={!errors[FIELD_TYPE.YEAR]}
-            />
-            <CustomInput
-              className="form__input"
-              onChange={handleChange}
-              name={FIELD_TYPE.PRICE}
-              label={DICTIONARY[FIELD_TYPE.PRICE]}
-              value={values[FIELD_TYPE.PRICE]}
-              isTouched={touched[FIELD_TYPE.PRICE]}
-              error={errors[FIELD_TYPE.PRICE]}
-              isValid={!errors[FIELD_TYPE.PRICE]}
-              type="number"
-            />
-          </div>
-          <div className="form__row">
-            <CustomInput
-              className="form__input"
               onChange={handleChange}
               name={FIELD_TYPE.DESCRIPTION}
               label={DICTIONARY[FIELD_TYPE.DESCRIPTION]}
@@ -101,42 +98,38 @@ export const AddCarForm = (props: IProps) => {
               isValid={!errors[FIELD_TYPE.DESCRIPTION]}
             />
           </div>
-          <div className="form__row">
-            <div className="form__color">
-              <span className="form__label">
-                {DICTIONARY[FIELD_TYPE.COLOR]}
-              </span>
-              {RADIO_COLORS.map((color: CAR_COLOR) => (
-                <CustomRadioInput
-                  className="form__radio"
-                  name={FIELD_TYPE.COLOR}
-                  key={color}
-                  value={color}
-                  onChange={handleChange}
-                  isChecked={color === values[FIELD_TYPE.COLOR]}
-                />
-              ))}
-            </div>
-            <CustomSelect
-              className="form__select form__status"
-              options={STATUS_OPTIONS}
-              name={FIELD_TYPE.STATUS}
-              label={DICTIONARY[FIELD_TYPE.STATUS]}
-              value={values[FIELD_TYPE.STATUS]}
-              setFieldValue={setFieldValue}
-              isTouched={touched[FIELD_TYPE.STATUS]}
-              error={errors[FIELD_TYPE.STATUS]}
-              isValid={!errors[FIELD_TYPE.STATUS]}
-            />
-
-            <button
-              className="form__submit btn"
-              type="submit"
-              disabled={!isValid}
-            >
-              {DICTIONARY.SUBMIT}
-            </button>
+          <div className="form__color">
+            <span className="form__label">{DICTIONARY[FIELD_TYPE.COLOR]}</span>
+            {RADIO_COLORS.map((color: CAR_COLOR) => (
+              <CustomRadioInput
+                className="form__radio"
+                name={FIELD_TYPE.COLOR}
+                key={color}
+                value={color}
+                onChange={handleChange}
+                isChecked={color === values[FIELD_TYPE.COLOR]}
+              />
+            ))}
           </div>
+          <CustomSelect
+            className="form__status"
+            options={STATUS_OPTIONS}
+            name={FIELD_TYPE.STATUS}
+            label={DICTIONARY[FIELD_TYPE.STATUS]}
+            value={values[FIELD_TYPE.STATUS]}
+            setFieldValue={setFieldValue}
+            isTouched={touched[FIELD_TYPE.STATUS]}
+            error={errors[FIELD_TYPE.STATUS]}
+            isValid={!errors[FIELD_TYPE.STATUS]}
+          />
+
+          <button
+            className="form__submit btn"
+            type="submit"
+            disabled={!isValid}
+          >
+            <span className="form__btn-text">{DICTIONARY.SUBMIT}</span>
+          </button>
         </form>
       )}
     />
